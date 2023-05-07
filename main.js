296a8c7b6e4bbb54f68295460ec8a309b8cf53e6
Igor Coelho <119017211+xH4rdzn@users.noreply.github.com>
2023-05-07 14:44:58 -0300

    Finalizando Exercicio

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicaNumero(a, b) {
+    return a * b;
+}
+function dizOi(name) {
+    return "Olá " + name;
+}
+console.log(multiplicaNumero(5, 5));
+console.log(dizOi("Igor"));
---
+++ main.ts
@@ -0,0 +1,10 @@
+function multiplicaNumero (a: number, b: number) {
+  return a * b
+}
+
+function dizOi (name: string) {
+  return "Olá " + name
+}
+
+console.log(multiplicaNumero(5,5));
+console.log(dizOi("Igor"));
